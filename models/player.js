34f980d73ba250c9a1b39db40d8276dfc4266040
CodeBrew28 <arshiam2@illinois.edu>
2018-06-22 13:33:06 -0700

    init

---
+++ models/player.js
@@ -0,0 +1,7 @@
+const { Schema, SchemaTypes } = require('mongoose');
+
+const PlayerSchema = new Schema({
+    email: { type: String, required: true, unique: true }
+})
+  
+module.exports = m => m.model('Player', PlayerSchema)
